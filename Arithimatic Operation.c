c454773e43543f5d8210c3b80cfb968c2813e36f
arpitsharma-Aa <115688997+arpitsharma-Aa@users.noreply.github.com>
2023-02-21 07:28:11 +0530

    Add files via upload

---
+++ Arithimatic Operation.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+ void main()
+ { int a,b, substract,multiply,divide;
+ printf ("enter the value &a\n");
+ scanf ("%d",&a);
+  printf ("enter the value &b\n");
+ scanf ("%d",&b);
+ substract= a-b;
+ multiply= a*b;
+ divide = a/b;
+  printf("Substract= %d\n",substract);
+   printf("Multiply= %d\n",multiply);
+    printf("Divide= %d\n",divide );
+ }
+ 
+ 
